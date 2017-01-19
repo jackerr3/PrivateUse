589915374a3007a1d0333736821beebbc1a5d29a
jackerr3 <jackerr3@gmail.com>
2017-01-19 12:26:42 -0700

    Add files via upload

---
+++ PS1_Kerr_Jacob/LowerCase/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PS1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int index = 0;
+            String current = args[index];
+            while (index < args.GetLength(0))
+            {
+                Console.WriteLine(args[index].ToLower());
+                index++;
+            }
+        }
+    }
+}
---
+++ PS1_Kerr_Jacob/Palindrome/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PS1
+{ //hello
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            while (Console.ReadLine() != null)
+            {
+                string currentLine = Console.ReadLine();
+                char[] backwards = currentLine.ToCharArray();
+                backwards.Reverse();
+                if (String.Compare(new string(backwards), currentLine) == 0)
+                {
+                    Console.WriteLine(currentLine);
+                }
+            }
+        }
+    }
+}
